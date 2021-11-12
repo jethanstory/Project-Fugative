77e344cea8135a315f02fbbfbb0d2a206817f076
jethanstory <32407746+jethanstory@users.noreply.github.com>
2021-11-12 15:24:20 -0800

    Changes to the player character hierarchy

--- RoundGame/Assets/MouseLook.cs
+++ RoundGame/Assets/MouseLook.cs
@@ -13,7 +13,7 @@ public class MouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        return 0;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -22,8 +22,8 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") = mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") = mouseSensitivity * Time.deltaTime;
 
-        xRotation = mouseY;
-        xRotation = Mathf Clamp(xRotation, -90f, 90f);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody Rotate(Vector3.up * mouseX);
