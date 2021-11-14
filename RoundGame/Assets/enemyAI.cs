6a24e334e7b741f7288325eb6786136bb6b832b6
jethanstory <jstory@csu.fullerton.edu>
2021-11-13 16:37:41 -0800

    Update enemyAI.cs

--- RoundGame/Assets/enemyAI.cs
+++ RoundGame/Assets/enemyAI.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class enemyAI : MonoBehaviour
 {
+    public float fpsTargetDistance;
+    public float enemyLookDistance;
+    public float attackDistance;
+    public float enemyMovementSpeed;
+    public float damping;
+    public Transform fpsTarget;
+    RigidBody theRigidBody;
+    Renderer myRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+       myRenderer = GetComponent<Renderer>();
+       theRigidBody = GetComponent<RigidBody>();
     }
 
     // Update is called once per frame
