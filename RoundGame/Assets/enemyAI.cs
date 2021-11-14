7ecc1ac1dffbd716b4d28f6fcb5e900a84dc5f8d
jethanstory <32407746+jethanstory@users.noreply.github.com>
2021-11-13 17:01:46 -0800

    Basic enemy AI implemented

--- RoundGame/Assets/enemyAI.cs
+++ RoundGame/Assets/enemyAI.cs
@@ -10,18 +10,39 @@ public class enemyAI : MonoBehaviour
     public float enemyMovementSpeed;
     public float damping;
     public Transform fpsTarget;
-    RigidBody theRigidBody;
+    Rigidbody theRigidBody;
     Renderer myRenderer;
     // Start is called before the first frame update
     void Start()
     {
        myRenderer = GetComponent<Renderer>();
-       theRigidBody = GetComponent<RigidBody>();
+       theRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        fpsTargetDistance = Vector3.Distance(fpsTarget.position, transform.position);
+        if (fpsTargetDistance < enemyLookDistance) {
+            myRenderer.material.color = Color.yellow;
+            lookAtPlayer();
+
+        }
+        if (fpsTargetDistance < attackDistance) {
+            myRenderer.material.color = Color.red;
+            attackPlease();
+        }
+        else{
+            myRenderer.material.color = Color.blue;
+        }
+    }
+
+    void lookAtPlayer() {
+        Quaternion rotation = Quaternion.LookRotation(fpsTarget.position - transform.position);
+        transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * damping);
+    }
+
+    void attackPlease() {
+        theRigidBody.AddForce(transform.forward * enemyMovementSpeed);
     }
 }
