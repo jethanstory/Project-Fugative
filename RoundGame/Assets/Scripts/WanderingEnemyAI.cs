d05420f5ab9b38e49b2c1ae330f353d330001d77
jethanstory <jstory@csu.fullerton.edu>
2021-11-16 20:38:27 -0800

    Basic Wandering AI enemy with flashlight

---
+++ RoundGame/Assets/Scripts/WanderingEnemyAI.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingEnemyAI : MonoBehaviour
+{
+    public float fpsTargetDistance;
+    public float enemyLookDistance;
+    public float attackDistance;
+    public float enemyMovementSpeed;
+    public float damping;
+
+    public GameObject lightSource;
+    public Transform fpsTarget;
+    Rigidbody theRigidBody;
+    Renderer myRenderer;
+
+    
+
+    //private Light enemyLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+       myRenderer = GetComponent<Renderer>();
+       theRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fpsTargetDistance = Vector3.Distance(fpsTarget.position, transform.position);
+        if (fpsTargetDistance < enemyLookDistance) {
+            myRenderer.material.color = Color.yellow;
+            lookAtPlayer();
+            if (fpsTargetDistance < attackDistance) {
+                myRenderer.material.color = Color.red;
+                attackPlease();
+            }
+        }
+        
+        else{
+            myRenderer.material.color = Color.blue;
+            //enemyLight.color = Color.white;
+          
+        }
+    }
+
+    void lookAtPlayer() {
+        Quaternion rotation = Quaternion.LookRotation(fpsTarget.position - transform.position);
+        transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * damping);
+         
+
+            /*else if (isOn == true)
+            {
+                lightSource.SetActive(false);
+                isOn = false;
+            }*/
+    }
+
+    void attackPlease() {
+        theRigidBody.AddForce(transform.forward * enemyMovementSpeed);
+        //enemyLight.color = Color.red;
+        //lightSource.SetActive(true);
+    }
+}
+
