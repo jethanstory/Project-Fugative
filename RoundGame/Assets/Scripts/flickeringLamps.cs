141ae1e4a8dc4515481c0dc7fe00a4d9df884b76
jethanstory <32407746+jethanstory@users.noreply.github.com>
2021-11-22 23:06:14 -0800

    Flickering added to flashlight

--- RoundGame/Assets/Scripts/flickeringLamps.cs
+++ RoundGame/Assets/Scripts/flickeringLamps.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class flickeringLamps : MonoBehaviour
 {
+
+    public float fpsTargetDistance;
+    public float enemyLookDistance;
+    public float attackDistance;
+    public float enemyMovementSpeed;
+    public float damping;
+
+    public Transform fpsTarget;
+    public bool isFlickering;
+    public int FlickerMode;
+    public float FlickerTime;
+
+    public GameObject lightSource;
+
+    public Light light;
+
+
+    
+    public float RandomIntensity;
+   
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +34,31 @@ public class flickeringLamps : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        fpsTargetDistance = Vector3.Distance(fpsTarget.position, transform.position);
+        if (fpsTargetDistance < enemyLookDistance) {
+            isFlickering = true;
+            StartCoroutine(FlickerLight());
+        }
+
+    }
+
+
+    IEnumerator FlickerLight()
+    {
+        if (FlickerMode == 1)
+        {
+            //this.gameObject.GetComponent<Light>().enabled = false;
+            lightSource.SetActive(false);
+            FlickerTime = Random.Range(0f, 0.26f);
+            RandomIntensity = Random.Range(0f, 3.1f);
+            light.intensity = RandomIntensity;
+            FlickerTime = Random.Range(0f, 0.05f);
+            yield return new WaitForSeconds(FlickerTime);
+            RandomIntensity = Random.Range(0f, 3.1f);
+            light.intensity = RandomIntensity;
+            lightSource.SetActive(true);
+            //this.gameObject.GetComponent<Light>().enabled = true;
+            isFlickering = false;
+        }
     }
 }
